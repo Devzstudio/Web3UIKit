0fd098c087428391d11067edbcbc623adbf18b92
Pjijin <pjijin1@gmail.com>
2021-09-29 12:53:32 +0530

    🐛 Fix: deploy error

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,19 +1,15 @@
+// @ts-nocheck
 import Document, { Head, Main, NextScript } from 'next/document';
-// Import styled components ServerStyleSheet
 import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
 	static getInitialProps({ renderPage }) {
-		// Step 1: Create an instance of ServerStyleSheet
 		const sheet = new ServerStyleSheet();
 
-		// Step 2: Retrieve styles from components in the page
 		const page = renderPage((App) => (props) => sheet.collectStyles(<App {...props} />));
 
-		// Step 3: Extract the styles as <style> tags
 		const styleTags = sheet.getStyleElement();
 
-		// Step 4: Pass styleTags as a prop
 		return { ...page, styleTags };
 	}
 
@@ -21,9 +17,9 @@ export default class MyDocument extends Document {
 		return (
 			<html>
 				<Head>
-					<title>My page</title>
-					{/* Step 5: Output the styles in the head  */}
-					{this.props.styleTags}
+					<title>Web3UIKit</title>
+
+					{this.props?.styleTags as any}
 				</Head>
 				<body>
 					<Main />
